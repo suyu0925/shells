f7bb1e3a218e2881ce82d3e08d7b0887ca40d364
suyu0925 <suyu0925@gmail.com>
2018-08-12 03:47:43 +0800

    selenium

---
+++ selenium.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ `id -u` -eq 0 ];then
+  echo "has sudo permission"
+else
+  echo "has not sudo permission"
+  exit 1
+fi
+
+firefox() {
+  ## Firefox and geckodriver
+  sudo apt-get install -y firefox
+
+  cd /tmp
+
+  wget https://github.com/mozilla/geckodriver/releases/download/v0.21.0/geckodriver-v0.21.0-linux64.tar.gz
+
+  tar xzvf geckodriver-v0.21.0-linux64.tar.gz
+
+  sudo cp ./geckodriver /usr/local/bin
+}
+
+chromium() {
+  ## Chromium and chromedriver
+  sudo apt-get install -y chromium
+
+  cd /tmp
+
+  wget https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip
+
+  # NOTICE: can not reach googleapis.com, abandon chromium
+}
+
+firefox

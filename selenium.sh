79839360fcb1c30979ad62f95ef31aef00075234
suyu0925 <suyu0925@gmail.com>
2018-08-12 04:03:13 +0800

    selenium

--- selenium.sh
+++ selenium.sh
@@ -11,6 +11,14 @@ firefox() {
   ## Firefox and geckodriver
   sudo apt-get install -y firefox
 
+  geckodriver_path=$(which geckodriver)
+  if [ ! -n "$geckodriver_path" ]; then
+    echo "start install geckodriver..."
+  else
+    echo "geckodriver has already installed on "$geckodriver_path
+    return 0
+  fi
+
   cd /tmp
 
   wget https://github.com/mozilla/geckodriver/releases/download/v0.21.0/geckodriver-v0.21.0-linux64.tar.gz

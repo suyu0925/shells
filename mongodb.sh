bd49e2a6ceabc1d32ebbb850d450b69a0f9990d7
suyu0925 <suyu0925@gmail.com>
2018-08-11 17:58:02 +0800

    mongodb

---
+++ mongodb.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Import the public key used by the package management system.
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+
+# Create a list file for MongoDB.
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+
+sudo apt-get update
+
+sudo apt-get install -y mongodb-org
+
+# Start MongoDB
+sudo service mongod start
+
+# Verify
+tail /var/log/mongodb/mongod.log

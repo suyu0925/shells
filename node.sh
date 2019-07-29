51ba5c880c31dee6f37df6fafe031d5aa5b756ee
suyu0925 <suyu0925@gmail.com>
2019-07-30 03:13:41 +0800

    update node to 10.x

--- node.sh
+++ node.sh
@@ -7,7 +7,7 @@ else
   exit 1
 fi
 
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 # install build tools
